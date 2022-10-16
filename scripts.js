510eed544079ec42646773dcf4c4933dade0eb76
AndrewWitsoe <andrewwitsoe@gmail.com>
2022-10-16 23:56:27 +0000

    added other recipes from other students into the project. Updated JS

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-// Javascript for TP6 
+// Javascript for TP7 
 // Also working on tinkering with functions and adding more customization to the website. WORK IN PROGRESS
 
 //function to load a file from the URL "fromfile" into the object identified by "whereto" 
@@ -24,4 +24,45 @@ function loadFileInto(fromFile, whereTo) {
 	// initiate request and wait for response
 	ajax.send();
 
+}
+
+// new recipe object constructor 
+
+function Recipe(recipeName, contributorName, imageURL, ingredientsURL, equipmentURL, directionsURL) {
+  
+  this.recipeName = recipeName;
+  this.contributor =  contributorName;
+  this.ingredients = ingredientsURL;
+  this.equipment = equipmentURL;
+  this.directions = directionsURL;
+  this.imageURL = imageURL;
+  
+  this.displayRecipe = function() {
+    
+    document.querySelector("#titleBanner h1").innerHTML = this.recipeName;
+    document.querySelector("#contributor").innerHTML = this.recipeName;
+    document.querySelector("#titleBanner").style.backgroundimage = "frenchtoast.jpg" + this.imageURL; 
+    loadFileInto(this.ingredients,"#ingredients ul");
+    loadFileInto(this.equipment,"#equipment ul");
+    loadFileInto(this.directions,"#directions ol");
+  }
+}
+
+FrenchToast = new Recipe("French Toast", "Andrew Witsoe", "frenchtoast.jpg", "ingredients.html", "equipment.html", "directions.html");
+
+LemonBars = new Recipe("Lemon Bars", "Coby Nelson", "", "LBingredients.html", "LBequipment.html", "LBdirections.html");
+
+SugarCookies = new Recipe("Maple Brown Sugar Cookies", "Kailin Gilzow", "", "SCingredients.html", "SCequipment.html", "SCdirections.html");
+
+window.onload = function() {
+  
+  document.querySelector("first").onclick = function() {
+    FrenchToast.displayRecipe();
+  }
+  document.querySelector("second").onclick = function() {
+    LemonBars.displayRecipe();
+  }
+  document.querySelector("third").onclick = function() {
+    SugarCookies.displayRecipe();
+  }
 }
